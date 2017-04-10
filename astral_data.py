fb5e2f1f4cb58f37b85173981af00ed5339f0c52
jhershberger <justin.hershberger3@gmail.com>
2017-04-09 22:04:47 -0600

    Updated the astral data to display the correct time and now include the solar noon and midnight

--- astral_data.py
+++ astral_data.py
@@ -2,7 +2,7 @@
 # @Date:   04-04-2017
 # @Filename: astral_data.py
 # @Last modified by:   Justin Hershberger
-# @Last modified time: 05-04-2017
+# @Last modified time: 09-04-2017
 
 
 
@@ -23,15 +23,16 @@ a = Astral()
 se_file = open('Astral Data/'+ str(yesterday.date()) + '-' + str(today.date()), 'w')
 se_file.write("Elevation and Azimuth Data from " + str(yesterday.date()) + " to " + str(today.date()) + "\n\n" )
 
-a.solar_depression = 'civil'
+se_file.write("Solar Noon: " + str(a.solar_noon_utc(date=today,longitude=lon)) + "\n\n")
+se_file.write("Solar Midnight: " + str(a.solar_midnight_utc(date=today,longitude=lon)) + "\n\n")
 
 # there are 1440 minutes in a day
 for i in range(1440):
     azimuth = a.solar_azimuth(dateandtime=(today - timedelta(0,0,0,0,i)), latitude=lat, longitude=lon)
     se = a.solar_elevation(dateandtime=(today - timedelta(0,0,0,0,i)), latitude=lat, longitude=lon)
     se_file.write("########################################################\n")
-    se_file.write(str(yesterday + timedelta(0,0,0,0,i)) + "- elevation: " + str(se) + "\n")
-    se_file.write(str(yesterday + timedelta(0,0,0,0,i)) + "- azimuth: " + str(azimuth) + "\n")
+    se_file.write(str(today - timedelta(0,0,0,0,i)) + "- elevation: " + str(se) + "\n")
+    se_file.write(str(today - timedelta(0,0,0,0,i)) + "- azimuth: " + str(azimuth) + "\n")
     se_file.write("########################################################\n\n")
 
 se_file.close()
