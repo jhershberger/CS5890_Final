5c64b1aa338fa7d14e902365c034d496e4040ee0
jhershberger <justin.hershberger3@gmail.com>
2017-04-26 20:29:46 -0600

    Reformatted the hourly breakdowns of data

--- data.py
+++ data.py
@@ -2,7 +2,7 @@
 # @Date:   27-03-2017
 # @Filename: data.py
 # @Last modified by:   Justin Hershberger
-# @Last modified time: 09-04-2017
+# @Last modified time: 26-04-2017
 
 
 
@@ -18,8 +18,9 @@ client = MongoClient()
 db = client.CS5890_Solar
 
 # this gets today's date and yesterday's so we can get yesterday's data each day
-today = str((datetime.today() - timedelta(2)).date())
-yesterday = str((datetime.today() - timedelta(3)).date())
+# today = str((datetime.today() - timedelta(2)).date())
+# yesterday = str((datetime.today() - timedelta(3)).date())
+
 
 def usu_climate_api():
     total = 0
@@ -69,7 +70,7 @@ def noaa_api():
 def openweather_api():
     # this uses the CS5890 token for Logan, UT
     #  I need to update this to get more info, maybe a forecast
-    url = "http://api.openweathermap.org/data/2.5/weather?id=5777544&APPID=1966474faa754fc3b6656f04bbeaa2a7"
+    url = "http://api.openweathermap.org/data/2.5/forecast?id=5777544&APPID=1966474faa754fc3b6656f04bbeaa2a7"
     r = requests.get(url)
     print r.json()
 
@@ -83,7 +84,100 @@ def nrel_api():
     print r.json()
 
 
-usu_climate_api()
+def postDay():
+    today = str((datetime.today()).date())
+    yesterday = str((datetime.today() - timedelta(1)).date())
+    postHourly(yesterday=yesterday, today=today)
+    # try:
+    #     p = db.disp
+    # except Post.DoesNotExist: # if the date isn't in the database then add it
+
+
+def postHourly(yesterday, today):
+    fl = open('C:/Users/Justin/Documents/GitHub/CS5890_Final/Hourly_Data/'+ yesterday + '-' + today, 'w')
+    fl.write("Hourly Data For: " + yesterday + " to " + today + '\n\n\n')
+    url = "https://climate.usurf.usu.edu/API/api.php/v1/key=TESTKEY/stationSrch/stationId=1266802/getHourly/startDate=" + yesterday + "/endDate="+ today +"/units=english"
+    r = requests.get(url)
+    jsn = r.json()
+    solar = {}
+    day_hr = []
+    day_hr_watt = []
+    day_hr_air_temp = []
+    solar_watts = {}
+    air_temp = {}
+    # this gets the solar radiaion for each day
+    for key in jsn:
+        if key == 'payload':
+            for ky in jsn[key]:
+                if 'solarmj' in ky:
+                    # print(ky['solarmj'])
+                    solar[ky['date_time']] = ky['solarmj']
+
+                    for el in solar:
+                        # print el,solar[el]
+                        day = datetime.strptime(el, '%Y-%m-%d %H:%M:%S')
+                        day_hr.append([datetime.strftime(day, '%H:%M'), str(solar[el]), '<br>'])
+                        # fl.write("Solar Radiation in Mj/m^2: " + str(datetime.strftime(day, '%H:%M:%S')) + ": " + str(solar[el]) + '\n')
+                        #this inserts the solar radiation for the day to mongo
+                if 'solar' in ky:
+                    solar_watts[ky['date_time']] = ky['solar']
+
+                    for el in solar_watts:
+                        # print el,solar[el]
+                        day = datetime.strptime(el, '%Y-%m-%d %H:%M:%S')
+                        day_hr_watt.append([datetime.strftime(day, '%H:%M:%S'), str(solar_watts[el]), '<br>'])
+                        # fl.write("Solar Radiation in W/m^2: " + str(datetime.strftime(day, '%H:%M:%S')) + ": " + str(solar_watts[el]) + '\n')
+                if 'airt_avg' in ky:
+                    air_temp[ky['date_time']] = ky['airt_avg']
+
+                    for el in air_temp:
+                        # print el,solar[el]
+                        day = datetime.strptime(el, '%Y-%m-%d %H:%M:%S')
+                        day_hr_air_temp.append([datetime.strftime(day, '%H:%M:%S'), str(air_temp[el]), '<br>'])
+                        # fl.write('Air Temp Avg: ' + str(datetime.strftime(day, '%H:%M:%S')) + ": " + str(air_temp[el]) + '\n')
+
+    #this gets a sorted version of the hourly breakdowns
+    new_day_hr = list()
+    new_day_hr_watts = list()
+    new_day_hr_air_temp = list()
+    map(lambda x: not x in new_day_hr and new_day_hr.append(x), day_hr)
+    map(lambda x: not x in new_day_hr_watts and new_day_hr_watts.append(x), day_hr_watt)
+    map(lambda x: not x in new_day_hr_air_temp and new_day_hr_air_temp.append(x), day_hr_air_temp)
+
+    new_day_hr_times = []
+    new_day_hr_data = []
+    fl.write('Solar Radiation in Mj/m^2\n')
+    for itm in new_day_hr:
+        new_day_hr_times.append(itm[0])
+        new_day_hr_data.append(itm[1])
+        fl.write(str(itm[0]) + '    ' + str(itm[1]) + '\n')
+
+
+    fl.write('\n\n')
+    fl.write('Air Temperature in Fahrenheit\n')
+    for itm in new_day_hr_air_temp:
+        fl.write(str(itm[0]) + '    ' + str(itm[1]) + '\n')
+
+    fl.close()
+
+
+    # result = Post.objects.create(
+    #     date_air_temp = new_day_hr_air_temp,
+    #     date_hr_mj = new_day_hr,
+    #     date_hr_watt = new_day_hr_watts,
+    #     date= datetime.strftime(day, '%Y-%m-%d'),
+    #     source= "Utah Climate Center",
+    #     station= "1266802",
+    #     solar_radiation= solar[el]
+    # )
+    #
+    #
+    # result.save()
+
+
+
+# usu_climate_api()
 # noaa_api()
 # openweather_api()
 # nrel_api()
+postDay()
